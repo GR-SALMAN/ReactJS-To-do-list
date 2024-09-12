47584023cf8d4861ae660963e8d3fb0c63e588e1
G.R. Salman <grsalman769@gmail.com>
2024-09-12 20:59:42 +0600

    animation update

--- src/list.jsx
+++ src/list.jsx
@@ -1,25 +1,28 @@
 import { useContext } from "react";
 import { ContextTag } from "./context/CreateContext";
-ContextTag;
 
-useContext;
 export const Listing = () => {
   const { notes, handleDelete, handleUpdate } = useContext(ContextTag);
+
   return (
     <>
-      <div className="output">
-        <ul>
-          {notes.map((list) => (
-            <li key={list.id}>
-              <strong className="title">{list.title}</strong>
-              <div className="btn-mini">
-                <button onClick={() => handleUpdate(list)}>Edit</button>
-                <button onClick={() => handleDelete(list.id)}>Delete</button>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {notes.length > 0 && (
+        <div className="output">
+          <ul>
+            {notes.map((list) => (
+              <li key={list.id} className="animated bounce">
+                <strong className="title">{list.title}</strong>
+                <div className="btn-mini">
+                  <button onClick={() => handleUpdate(list)}>Edit</button>
+                  <button onClick={() => handleDelete(list.id)}>Delete</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {/* Optionally, you can add an alternative message for no notes */}
+      {notes.length === 0 && <p>No notes available yet.</p>}
     </>
   );
 };
